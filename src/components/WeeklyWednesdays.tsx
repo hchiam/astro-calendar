fcfaf52e18a824146eadd039bb8da3babd43cedc
hchiam <hchiam@users.noreply.github.com>
2021-08-04 01:49:53 -0400

    fix WeeklyWednesdays (wasn't alternating)

--- src/components/WeeklyWednesdays.tsx
+++ src/components/WeeklyWednesdays.tsx
@@ -6,10 +6,15 @@ interface WeeklyWednesdays {
 
 export default function WeeklyWednesdays(props) {
   const { date } = props;
-  const isFourDateReference = new Date("2021-08-05T02:41:05.841Z");
+  const isFourDateReference = new Date("2021-08-04T00:00:00.000Z");
   const diff = Math.round(
-    (date.getTime() - isFourDateReference.getTime()) / 24 / 60 / 60 / 1000
+    Math.abs(date.getTime() - isFourDateReference.getTime()) /
+      24 /
+      60 /
+      60 /
+      1000
   );
+
   const isFive = diff % 14 !== 0;
   const isFour = diff % 14 === 0;
   const isFiveText = addBrackets("TGRNF");
