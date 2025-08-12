cfdee7919121f16c7fe3bd7f6be1090de0c13a29
hchiam <hchiam@users.noreply.github.com>
2025-08-12 16:55:59 -0600

    8 months, to get an even number of pages (4)

--- src/components/Calendar.tsx
+++ src/components/Calendar.tsx
@@ -6,7 +6,7 @@ import StatHolidaysWrapper from "../components/StatHolidays";
 
 export default function Calendar() {
   const today = new Date();
-  const [numberOfMonths, setNumberOfMonths] = useState(6);
+  const [numberOfMonths, setNumberOfMonths] = useState(8);
   return (
     <>
       {/* <input
